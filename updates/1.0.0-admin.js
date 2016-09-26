b4600fe8adc4c09929b9d7ffa638f8d929893f91
Releaseself <releaseself@outlook.com>
2016-09-26 19:10:49 +0800

    add Q

--- updates/1.0.0-admin.js
+++ updates/1.0.0-admin.js
@@ -1,11 +1,22 @@
-var keystone = require('keystone'),
-    User = keystone.list('User');
+var models = require('../models');
 
 exports = module.exports = function (done) {
-    new User.model({
-        name: {first: 'Admin', last: 'User'},
-        email: 'admin@liuxue.com',
-        password: 'admin',
-        canAccessKeystone: true
-    }).save(done);
+    models.shop.count(function (err, result) {
+        if (err) return done(err);
+        if (result > 0) return done();
+
+
+    })
+
+    function init() {
+        var staff = new models.staff({
+            name: 'test',
+            type: 'test',
+            price: 100,
+            original: 1000,
+            intro: 'This is a staff'
+        }).save(function (err, done) {
+
+        });
+    }
 };
